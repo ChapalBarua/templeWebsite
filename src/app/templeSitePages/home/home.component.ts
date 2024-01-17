5986c2f03480b429839e3f925e2c0211f3b2c3b2
ChapalBarua <chapalbuet@yahoo.com>
2024-01-17 16:00:32 -0500

    [temple-46] trying to fix build issue

--- src/app/templeSitePages/home/home.component.ts
+++ src/app/templeSitePages/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NotificationService, NotificationType } from 'src/app/Services/notification.service';
 import { ImagesListUrl, UploadService } from 'src/app/Services/upload.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
   public images: Record<string, string | number>[] = [
