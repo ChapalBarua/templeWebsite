5099775faa6bd894a5fead0d813171d887eaf278
ChapalBarua <chapalbuet@yahoo.com>
2024-01-17 18:55:21 -0500

    [temple-47] Load home component after images are loaded - fix carousel navigation bug

--- src/app/templeSitePages/home/home.component.ts
+++ src/app/templeSitePages/home/home.component.ts
@@ -1,28 +1,17 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { NotificationService, NotificationType } from 'src/app/Services/notification.service';
-import { ImagesListUrl, UploadService } from 'src/app/Services/upload.service';
+import { ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  encapsulation: ViewEncapsulation.None
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
-  public images: Record<string, string | number>[] = [
-  ];
+  
+  @Input()
+  images: Record<string, string | number>[] = [];
   public partialVisible: boolean = true;
-
-  constructor(private uploadService: UploadService, private notificationService: NotificationService) { 
-    this.uploadService.getFileList(ImagesListUrl).subscribe({
-      next: (result)=>{
-        this.images = result;
-      },
-      error: (error: any)=>{
-        this.notificationService.sendMessage({message: 'No Images Found: ' + error.error.msg, type: NotificationType.error});
-      }
-    });
-  }
 
   ngOnInit(): void {
   }
