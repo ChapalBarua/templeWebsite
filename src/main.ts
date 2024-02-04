c6cc1035a1834d72660879d210b11043f1bd555d
ChapalBarua <chapalbuet@yahoo.com>
2024-02-03 20:48:13 -0500

    [temple-56] Hide secret keys

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { registerLicense } from '@syncfusion/ej2-base';
+import { License_key } from "../../TempleServer/helpers/secrets/secret_keys_ui";
 
 if (environment.production) {
   enableProdMode();
 }
 
-
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWH1fc3VXRGNdWEZzX0E=');
+registerLicense(License_key);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
